7a456f65db93d6232c1d0115571a631af8e4dd5c
rhtmdwh <rhtmdwh19@gmail.com>
2024-01-19 18:40:22 +0900

    Partition Studiy

--- src/main/java/com/example/springbatchstudy/MultiThreadedJobConfiguration.java
+++ src/main/java/com/example/springbatchstudy/MultiThreadedJobConfiguration.java
@@ -12,14 +12,11 @@ import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.xml.transform.Result;
 
 @Slf4j
 //@Configuration
-public class MultiThreadedJobConfig {
+public class MultiThreadedJobConfiguration {
 
     @Bean
     public Job job(
--- src/main/java/com/example/springbatchstudy/ParallelStepJobConfiguration.java
+++ src/main/java/com/example/springbatchstudy/ParallelStepJobConfiguration.java
@@ -11,14 +11,13 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 
 @Slf4j
-@Configuration
-public class ParallelStepJobConfig {
+//@Configuration
+public class ParallelStepJobConfiguration {
 
 
     // flow 1 (step1, step2)
